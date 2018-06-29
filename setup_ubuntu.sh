87d07489f368aead3a4c5bce2f873db8f84c88bc
Simon Mo <xmo@berkeley.edu>
2018-06-29 15:02:48 -0700

    Update script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,8 +1,9 @@
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh &
-sudo apt update &
-sudo apt install build-essentials htop &
+set -x
 
-wait
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh &
+
+sudo apt update 
+sudo apt install -y build-essential htop &
 
 git config --global crendential.helper store
 git config --global user.name=simon-mo
@@ -10,5 +11,5 @@ git config --global user.email=xmo@berkeley.edu
 git config --global core.editor="vim"
 
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
-echo 'PATH="$HOME/miniconda/bin:$PATH"' >> .bashrc
+echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> .bashrc
 source .bashrc

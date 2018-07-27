b3cb2fdb51826909b9a93d060f61b6d4d128f1e8
simon-mo <xmo@berkeley.edu>
2018-07-27 20:29:02 +0000

    Fix miniconda3 path

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,7 +3,7 @@ set -x
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 
 sudo apt update 
-sudo apt install -y build-essential htop iperf3
+sudo apt install -y build-essential clang cmake htop iperf3
 
 git config --global crendential.helper store
 git config --global user.name simon-mo
@@ -11,7 +11,7 @@ git config --global user.email xmo@berkeley.edu
 git config --global core.editor vim
 
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> $HOME/.bashrc
+echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> $HOME/.bashrc
 source $HOME/.bashrc
 rm Miniconda3-latest-Linux-x86_64.sh
 
